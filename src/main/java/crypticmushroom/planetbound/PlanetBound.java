2c11b1270e3b59186fbc1b06778fdfe47a74eb66
Cipher Zero X <cipherzerox@gmail.com>
2018-01-10 17:32:38 -0600

    Redid the main mod file

--- src/main/java/crypticmushroom/planetbound/PlanetBound.java
+++ src/main/java/crypticmushroom/planetbound/PlanetBound.java
@@ -1,71 +1,53 @@
 package crypticmushroom.planetbound;
 
-import org.apache.logging.log4j.core.Logger;
-
-import crypticmushroom.planetbound.init.PBBlocks;
-import crypticmushroom.planetbound.init.PBItems;
 import crypticmushroom.planetbound.proxy.CommonProxy;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.world.DimensionType;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = PlanetBound.MODID, name = PlanetBound.MODNAME, version = PlanetBound.MODVERSION, dependencies = "required-after:forge@[14.23.1.2555,)", useMetadata = true)
+@Mod(modid = PlanetBound.MODID, 
+		name = "PlanetBound", 
+		version = PlanetBound.VERSION)
+
 public class PlanetBound {
 	
 	public static final String MODID = "PlanetBound";
-	public static final String MODNAME = "PlanetBound";
-	public static final String MODVERSION = "0.0.1";
+	public static final String VERSION = "1.0";
+	
+	public static final PBItems items = new PBItems();
+	public static final PBBlocks blocks = new PBBlocks();
+	public static final String ARMOR_DIR = "planetbound:textures/armor/";
 	
 	@SidedProxy(clientSide = "crypticmushroom.planetbound.proxy.ClientProxy", serverSide = "crypticmushroom.planetbound.proxy.ServerProxy")
 	public static CommonProxy proxy;
 	
-	@Mod.Instance
-	public static PlanetBound instance;
+	public static DimensionType dimType;
+	public static int backupdimensionID = -290;
 	
-	public static Logger logger;
-	
-	@Mod.EventHandler
+	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		logger = (Logger) event.getModLog();
-		proxy.preInit(event);
+		
 	}
 	
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent e) {
-		proxy.init(e);
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
 	}
 	
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent e) {
-		proxy.postInit(e);
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
 	}
 	
-	@Mod.EventBusSubscriber
-	public static class RegistrationHandler {
-		
-		@SubscribeEvent
-		public static void registerBlocks(RegistryEvent.Register<Block> event) {
-			PBBlocks.register(event.getRegistry());
-		}
-		
-		@SubscribeEvent
-		public static void registerItems(RegistryEvent.Register<Item> event) {
-			PBItems.register(event.getRegistry());
-			PBBlocks.registerItemBlocks(event.getRegistry());
-		}
-		
-		@SubscribeEvent
-		public static void registerModels(ModelRegistryEvent event) {
-			PBItems.registerItemModel();
-			PBBlocks.registerItemModels();
-		}
-	}
-
 }
