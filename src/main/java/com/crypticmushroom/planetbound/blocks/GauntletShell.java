974b176ecc7ac9777624b77c57907277e23af368
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-01-17 17:51:26 +0100

    Removed unused imports

--- src/main/java/com/crypticmushroom/planetbound/blocks/GauntletShell.java
+++ src/main/java/com/crypticmushroom/planetbound/blocks/GauntletShell.java
@@ -4,10 +4,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class GauntletShell extends Block
 {
