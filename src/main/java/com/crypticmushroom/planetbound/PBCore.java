079406616092f0e51886089b339b85f66042066b
Jonathan Colmenares <sansistypinggit@gmail.com>
2018-01-17 18:00:42 -0500

    Mod now checks for updates

--- src/main/java/com/crypticmushroom/planetbound/PBCore.java
+++ src/main/java/com/crypticmushroom/planetbound/PBCore.java
@@ -19,12 +19,17 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = PBCore.MOD_ID, name = PBCore.NAME, version = PBCore.VERSION, acceptedMinecraftVersions = "[1.12.2]")
+@Mod(modid = PBCore.MOD_ID,
+		name = PBCore.NAME,
+		version = PBCore.VERSION,
+		acceptedMinecraftVersions = "[1.12.2]",
+		updateJSON = PBCore.UPDATE_JSON)
 public class PBCore
 {
 	public static final String MOD_ID = "planetbound";
 	public static final String NAME = "Planet Bound";
 	public static final String VERSION = "1.0-dev";
+	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/cipherzerox/Planet-Bound/master/update.json";
 	
 	@Instance(MOD_ID)
 	private static PBCore instance;
