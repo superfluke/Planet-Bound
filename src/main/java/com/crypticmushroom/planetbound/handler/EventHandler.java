4aecfd34792b41d189e49c41ec6fbd5f5e21b107
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-01-17 12:27:11 +0100

    Register the event handler some other time

--- src/main/java/com/crypticmushroom/planetbound/handler/EventHandler.java
+++ src/main/java/com/crypticmushroom/planetbound/handler/EventHandler.java
@@ -1,11 +1,6 @@
 package com.crypticmushroom.planetbound.handler;
 
-import net.minecraftforge.common.MinecraftForge;
-
 public class EventHandler
 {
-    public void register()
-    {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+	
 }
