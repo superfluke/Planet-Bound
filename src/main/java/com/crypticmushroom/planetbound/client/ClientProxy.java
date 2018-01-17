78be10cd0b101d4e5e97527e74f89927afa72c31
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-01-17 12:25:36 +0100

    Moved proxy classes to appropriate packages

--- src/main/java/com/crypticmushroom/planetbound/client/ClientProxy.java
+++ src/main/java/com/crypticmushroom/planetbound/client/ClientProxy.java
@@ -1,9 +1,15 @@
-package com.crypticmushroom.planetbound.proxy;
+package com.crypticmushroom.planetbound.client;
+
+import com.crypticmushroom.planetbound.CommonProxy;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
+//Server side only stuff in CLIENT package.
 public class ClientProxy extends CommonProxy
 {
 	@Override
--- src/main/java/com/crypticmushroom/planetbound/server/ServerProxy.java
+++ src/main/java/com/crypticmushroom/planetbound/server/ServerProxy.java
@@ -1,9 +1,15 @@
-package com.crypticmushroom.planetbound.proxy;
+package com.crypticmushroom.planetbound.server;
+
+import com.crypticmushroom.planetbound.CommonProxy;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.SERVER)
+//Server side only stuff in SERVER package.
 public class ServerProxy extends CommonProxy
 {
 	@Override
