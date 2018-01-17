5ac1ea6b29a54b6681520df04e6d1f79c1591587
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-01-17 19:36:14 +0100

    Added a custom creative tab

---
+++ src/main/java/com/crypticmushroom/planetbound/init/PBCreativeTabs.java
@@ -0,0 +1,17 @@
+package com.crypticmushroom.planetbound.init;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
+public class PBCreativeTabs
+{
+	//TODO temporary test tab; will be removed in the future
+	public static final CreativeTabs TAB_MAIN = new CreativeTabs("main")
+	{
+	    @Override 
+	    public ItemStack getTabIconItem()
+	    {
+	        return new ItemStack(PBItems.verdanite_ingot);
+	    }
+	};
+}
