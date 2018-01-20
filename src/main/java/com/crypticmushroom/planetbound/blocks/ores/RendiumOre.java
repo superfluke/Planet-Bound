0303cc104112fda1da8bec10c15c03a24d201da0
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-01-20 15:18:26 +0100

    Moved block break stuff to block class itself

--- src/main/java/com/crypticmushroom/planetbound/blocks/ores/RendiumOre.java
+++ src/main/java/com/crypticmushroom/planetbound/blocks/ores/RendiumOre.java
@@ -1,10 +1,15 @@
 package com.crypticmushroom.planetbound.blocks.ores;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.crypticmushroom.planetbound.init.PBItems;
 
 import net.minecraft.block.BlockOre;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class RendiumOre extends BlockOre
 {
@@ -20,6 +25,34 @@ public class RendiumOre extends BlockOre
     @Override
     public Item getItemDropped(IBlockState state, Random random, int fortune)
     {
-        return null;
+        return PBItems.rendium_chunk;
+    }
+    
+    @Override
+    public int quantityDropped(Random random)
+    {
+        return ThreadLocalRandom.current().nextInt(3, 5 + 1);
+    }
+    
+    @Override
+    public int quantityDroppedWithBonus(int fortune, Random random)
+    {
+        if(fortune > 0 && Item.getItemFromBlock(this) != this.getItemDropped((IBlockState)this.getBlockState().getValidStates().iterator().next(), random, fortune))
+        {
+            float max = 5 + fortune * 1.5F;
+            int drops = ThreadLocalRandom.current().nextInt(3, (int)(max + 1));
+            
+            return drops;
+        }
+        else
+        {
+            return this.quantityDropped(random);
+        }
+    }
+    
+    @Override
+    public void dropBlockAsItemWithChance(World worldIn, BlockPos pos, IBlockState state, float chance, int fortune)
+    {
+        super.dropBlockAsItemWithChance(worldIn, pos, state, chance, fortune);
     }
 }
--- src/main/java/com/crypticmushroom/planetbound/handler/BlockEvents.java
+++ src/main/java/com/crypticmushroom/planetbound/handler/BlockEvents.java
@@ -1,25 +1,6 @@
 package com.crypticmushroom.planetbound.handler;
 
-import java.util.concurrent.ThreadLocalRandom;
-
-import com.crypticmushroom.planetbound.init.PBBlocks;
-import com.crypticmushroom.planetbound.init.PBItems;
-
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 public class BlockEvents
 {
-    @SubscribeEvent
-    public void onHarvestDrops(HarvestDropsEvent event)
-    {
-        if(event.getState().getBlock().equals(PBBlocks.rendium_ore))
-        {
-            float max = 5 + event.getFortuneLevel() * 1.5F;
-            int drops = ThreadLocalRandom.current().nextInt(3, (int)(max + 1));
-            
-            event.getDrops().add(new ItemStack(PBItems.rendium_chunk, drops));
-        }
-    }
+    
 }
