f7b1ff417d93f641c5b857d2e44c9438d4efcd5e
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-01-22 20:46:32 +0100

    Added a smelting recipe for rendium crystals and added xp rewards

--- src/main/java/com/crypticmushroom/planetbound/init/PBSmelting.java
+++ src/main/java/com/crypticmushroom/planetbound/init/PBSmelting.java
@@ -8,7 +8,8 @@ public class PBSmelting
     //TODO add xp values for smelting
     public static void init()
     {
-        FurnaceRecipes.instance().addSmeltingRecipeForBlock(PBBlocks.kybrite_ore, new ItemStack(PBItems.kybrite_ingot), 0);
-        FurnaceRecipes.instance().addSmeltingRecipeForBlock(PBBlocks.verdanite_ore, new ItemStack(PBItems.verdanite_ingot), 0);
+        FurnaceRecipes.instance().addSmeltingRecipeForBlock(PBBlocks.kybrite_ore, new ItemStack(PBItems.kybrite_ingot), 0.7F);
+        FurnaceRecipes.instance().addSmeltingRecipeForBlock(PBBlocks.verdanite_ore, new ItemStack(PBItems.verdanite_ingot), 0.7F);
+        FurnaceRecipes.instance().addSmelting(PBItems.rendium_chunk, new ItemStack(PBItems.rendium_crystal), 0.7F);
     }
 }
