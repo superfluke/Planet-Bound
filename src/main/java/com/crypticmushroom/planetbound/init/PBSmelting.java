39bf6faf21c6e348664b89c385bada295acb9d39
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-01-24 17:39:19 +0100

    Removed todo

--- src/main/java/com/crypticmushroom/planetbound/init/PBSmelting.java
+++ src/main/java/com/crypticmushroom/planetbound/init/PBSmelting.java
@@ -5,7 +5,6 @@ import net.minecraft.item.crafting.FurnaceRecipes;
 
 public class PBSmelting
 {
-    //TODO add xp values for smelting
     public static void init()
     {
         FurnaceRecipes.instance().addSmeltingRecipeForBlock(PBBlocks.kybrite_ore, new ItemStack(PBItems.kybrite_ingot), 0.7F);
