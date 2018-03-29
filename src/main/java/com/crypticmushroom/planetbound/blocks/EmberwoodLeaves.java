d966d089edb307b81331652e2ca38c113976b576
Dennis van Giessen <dennisvangiessen@hotmail.nl>
2018-03-29 21:16:24 +0200

    Shearing emberwood leaves now drops the leaves

--- src/main/java/com/crypticmushroom/planetbound/blocks/EmberwoodLeaves.java
+++ src/main/java/com/crypticmushroom/planetbound/blocks/EmberwoodLeaves.java
@@ -5,36 +5,20 @@ import java.util.Random;
 
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.block.BlockPlanks;
-import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import scala.actors.threadpool.Arrays;
 
 public class EmberwoodLeaves extends BlockLeaves
 {
-
     public EmberwoodLeaves()
     {
         super();
-
-        this.setHardness(0.2F);
-        this.setLightOpacity(1);
-        this.setSoundType(SoundType.PLANT);
-        this.setTickRandomly(false);
     }
-
-    @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getBlockLayer()
-    {
-        return BlockRenderLayer.CUTOUT;
-    }
-
+    
     @Override
     public BlockPlanks.EnumType getWoodType(int meta)
     {
@@ -46,23 +30,10 @@ public class EmberwoodLeaves extends BlockLeaves
     {
         return null;
     }
-
+    
     @Override
     public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune)
     {
-        return null;
+        return Arrays.asList(new ItemStack[]{new ItemStack(this)});
     }
-
-    @Override
-    public boolean isOpaqueCube(IBlockState state)
-    {
-    	return false;
-    }
-
-    @Override
-    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
-    {
-        return true;
-    }
-
 }
