11b813c91041e2113b21cd4c9a2c45e6cea21c79
raptor4694 <raptor4694@gmail.com>
2018-05-25 14:35:19 -0400

    Removed PBBlock base class as it was unnecessary

--- src/main/java/com/crypticmushroom/planetbound/blocks/PBStorageBlock.java
+++ src/main/java/com/crypticmushroom/planetbound/blocks/PBStorageBlock.java
@@ -1,10 +1,11 @@
 package com.crypticmushroom.planetbound.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 
-public class PBStorageBlock extends PBBlock {
+public class PBStorageBlock extends Block {
 	public PBStorageBlock(MapColor mapColorIn) {
 		super(Material.ROCK, MapColor.GRAY);
 
---
+++ src/main/java/com/crypticmushroom/planetbound/blocks/ores/PBOre.java
@@ -0,0 +1,14 @@
+package com.crypticmushroom.planetbound.blocks.ores;
+
+import net.minecraft.block.BlockOre;
+import net.minecraft.block.material.MapColor;
+
+public class PBOre extends BlockOre {
+	public PBOre() {
+
+	}
+
+	public PBOre(MapColor mapColorIn) {
+		super(mapColorIn);
+	}
+}
