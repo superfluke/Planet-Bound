f6c33f270a4a4002b066a7d8f94d1c922688bba7
Kino <jose33201@hotmail.com>
2018-06-09 16:27:43 -0400

    Players no longer spawn in the ground

--- src/main/java/com/crypticmushroom/planetbound/world/TeleporterRonne.java
+++ src/main/java/com/crypticmushroom/planetbound/world/TeleporterRonne.java
@@ -1,6 +1,7 @@
 package com.crypticmushroom.planetbound.world;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
 
@@ -9,6 +10,17 @@ public class TeleporterRonne extends Teleporter
     public TeleporterRonne(WorldServer world)
     {
         super(world);
+    }
+
+    @Override
+    public void placeInPortal(Entity entityIn, float rotationYaw)
+    {
+    	BlockPos pos = this.world.getTopSolidOrLiquidBlock(entityIn.getPosition()).up().east(2);
+
+        entityIn.setLocationAndAngles((double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), entityIn.rotationYaw, 0.0F);
+        entityIn.motionX = 0.0D;
+        entityIn.motionY = 0.0D;
+        entityIn.motionZ = 0.0D;
     }
 
     @Override
