a5ab7829a1d75ce968052aa813cc6fafd31c119c
UpcraftLP <j-schirra@gmx.de>
2018-08-04 05:48:01 +0200

    fixed TileEntity mapping

--- src/main/java/com/crypticmushroom/planetbound/init/PBTileEntities.java
+++ src/main/java/com/crypticmushroom/planetbound/init/PBTileEntities.java
@@ -1,5 +1,6 @@
 package com.crypticmushroom.planetbound.init;
 
+import com.crypticmushroom.planetbound.PBCore;
 import com.crypticmushroom.planetbound.tileentity.TileEntityInventorsForge;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -7,6 +8,6 @@ public class PBTileEntities
 {
     public static void init()
     {
-        GameRegistry.registerTileEntity(TileEntityInventorsForge.class, "inventors_forge");
+        GameRegistry.registerTileEntity(TileEntityInventorsForge.class, PBCore.MOD_ID + ":inventors_forge");
     }
 }
